1cb38a7b826fbecf78508ab110f1ea17e7b4475c
HruJungUn <hju7826@daum.net>
2019-05-07 21:31:05 +0900

    2019.05.07버튼과로그인페이지

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/login.css">
+    <title>로그인</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div id="mainimage">
+            <div class="filter">
+                <h1>Fund Manager</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsum aspernatur impedit iure, minus ut deleniti maxime placeat magnam, velit molestiae repudiandae iusto architecto. Illo porro sunt culpa a inventore.</p>
+            </div>
+        </div>
+        <div id="loginbox">
+            <h1>LOGIN</h1>
+            <form action="login_ok.php" method="post">
+                <input type="text" name="id" class="id" placeholder="ID">
+                <input type="password" name="password" class="password" placeholder="PASSWORD">
+                <button class="submit">LOGIN</button>
+            </form>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
